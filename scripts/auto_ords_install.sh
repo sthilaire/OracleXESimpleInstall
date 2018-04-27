c48bd91586052efa529706dd255b5c015240af9b
Mac VM <test@wphilltech.com>
2018-04-26 22:05:52 -0400

    upate to ORDS install for auto start - confirmed working

--- scripts/auto_ords_install.sh
+++ scripts/auto_ords_install.sh
@@ -4,16 +4,18 @@
 
 echo "... Unzip ords in directory = "$ZIPS_PATH
 
-unzip $ORDS_ZIP -d $ORDS_PATH
+unzip -q $ORDS_ZIP -d $ORDS_PATH
 
 echo "... copy default configuration"
 #cp -r $THIS_DIR/ords/config /u01/app/oracle/ords
 mkdir -p $ORDS_PATH/config/ords/standalone/doc_root
+cp -r $THIS_DIR/ords/doc_root $ORDS_PATH/config/ords/standalone/
 cp -r $THIS_DIR/ords/params $ORDS_PATH
 
 echo "*** TBD - SED the default parameter file to include passwords from param file ***"
 
 echo "... burn the configdir into the ords.war"
+# This is to cover the chance that someone starts the ORDS manually
 java -jar $ORDS_PATH/ords.war configdir $ORDS_PATH/config/
 
 if [[ "$APEX_UPGRADE" = "Y" ]]
@@ -22,10 +24,16 @@ then
     cp -r $ZIPS_PATH/working/apex/images $ORDS_PATH/apex_images
 fi
 
-echo "*** TBD - Set permissions to allow service to run ***"
+echo "... Set permissions to allow service to run as user $ORDSUSER"
+chown -R oracle:dba $ORDS_PATH
 
-echo "*** copy startup files ***"
-echo "*** start service ***"
+echo "... copy startup script"
+cp $THIS_DIR/ords/init.d/ords /etc/init.d
 
+echo "... update the systemctl registrations"
+systemctl daemon-reload
+
+echo "... start service"
+service ords start
 
 
