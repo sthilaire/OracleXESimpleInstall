6c16d083b2d2c25cd2faa42ad23b13f84100cf6a
sthilaire <sthilaire@wphilltech.com>
2018-05-16 09:24:07 -0400

    updates to script automation

--- scripts/xe_adjustments.sql
+++ scripts/xe_adjustments.sql
@@ -1,12 +1,23 @@
 rem Adjustments to Oracle XE
 
 -- Option - Change Default to Unlimited --
-ALTER PROFILE DEFAULT LIMIT PASSWORD_LIFE_TIME UNLIMITED;
+alter profile default limit password_life_time unlimited;
 
 alter user apex_public_user identified by password;
-
 alter user apex_rest_public_user identified by password;
+alter user apex_listener identified by password;
+alter user ords_public_user identified by password;
 
+alter user apex_public_user account unlock;
+
+-- EPG disable
+exec dbms_xdb.sethttpport(0);
+
+-- Add default Internal User
+
+-- Add Workspaces
+
+-- Adjust password complexity rules to be simple
 
 
 rem Be sure to leave
